f701a56665186cd90b6555c36c782151a269c937
stealhead <691523943@qq.com>
2025-01-26 16:12:06 +0800

    优化登录注册界面的UI

--- front/src/view/LoginView.vue
+++ front/src/view/LoginView.vue
@@ -1,97 +1,5 @@
-<template>
-  <!--  登录-->
-  <div id="login">
-    <el-form
-        ref="form"
-        style="max-width: 600px"
-        :model="newUser"
-        :rules="rules"
-        label-width="auto"
-        class="demo-ruleForm"
-        :size="formSize"
-        status-icon
-        v-if="isLogin"
-
-    >
-      <el-form-item label="账号" prop="account">
-        <el-input
-            v-model="newUser.account"
-            placeholder="请输入你的账号"
-            clearable
-
-        />
-      </el-form-item>
-
-
-      <el-form-item label="密码" prop="password">
-        <el-input
-            v-model="newUser.password"
-            style="width: 240px"
-            type="password"
-            placeholder="请输入你的密码"
-            show-password
-            clearable
-        />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="singIn" :disabled="unable">登录</el-button>
-        <el-button @click="toggle">返回注册</el-button>
-      </el-form-item>
-    </el-form>
-
-    <!--注册-->
-    <el-form
-        style="max-width: 600px"
-        :model="newUser"
-        :rules="rules"
-        label-width="auto"
-        class="demo-ruleForm"
-        :size="formSize"
-        status-icon
-        v-if="!isLogin"
-    >
-      <el-form-item label="昵称" prop="nickname">
-        <el-input
-            v-model="newUser.nickname"
-            style="width: 240px"
-            type="text"
-            placeholder="昵称"
-            clearable
-        />
-      </el-form-item>
-
-      <el-form-item label="密码" prop="password">
-        <el-input
-            v-model="newUser.password"
-            style="width: 240px"
-            type="password"
-            placeholder="请输入你的密码"
-            show-password
-            clearable
-        />
-      </el-form-item>
-
-      <el-form-item label="再次输入密码" prop="rePassword">
-        <el-input
-            v-model="newUser.rePassword"
-            style="width: 240px"
-            type="password"
-            placeholder="再次输入你的密码"
-            show-password
-            clearable
-        />
-      </el-form-item>
-
-      <el-form-item>
-        <el-button type="primary" @click="signUp">注册</el-button>
-        <el-button @click="toggle">返回登录</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
-</template>
-
 <script lang="ts" setup>
-import {reactive, ref, useTemplateRef} from 'vue'
+import {reactive, ref, watch} from 'vue'
 import type {ComponentSize, FormInstance, FormRules} from 'element-plus'
 import {login, register} from "@/api/user/UserApi";
 import {ElMessage} from "element-plus";
@@ -128,11 +36,30 @@ const toggle = () => {
   newUser.password = '';
 }
 // 密码长度
-const min_len_pwd = 3, max_len_pwd = 16;
+const min_len_pwd = 1, max_len_pwd = 16;
 // 昵称长度
 const min_len_nk = 1, max_len_nk = 10;
 // 账号长度
-const min_len_acc = 10, max_len_acc = 11;
+const min_len_acc = 3, max_len_acc = 11;
+
+
+const bot = ref('500px');
+watch(isLogin, () => {
+  // console.log(bot.value);
+  // bot.value = isLogin.value ? '500px' : '550px';
+  ad_word_h3.bottom = isLogin.value ? '500px' : '550px';
+  ad_word_h1.bottom = isLogin.value ? '550px' : '600px';
+});
+
+const ad_word_h3 = reactive({
+  position: 'fixed',
+  bottom: undefined ?? '500px',
+});
+
+const ad_word_h1 = reactive({
+  position: 'fixed',
+  bottom: undefined ?? '550px',
+})
 
 
 const validateRepass = (rule: object, value: string, callback: Function) => {
@@ -222,14 +149,8 @@ const rules = reactive<FormRules<NewUser>>({
 
 
 const singIn = () => {
-  // const form = useTemplateRef('form');
   form.value.validate(pass => {
-    if (!pass) {
-      ElMessage({
-        type: 'info',
-        message: `提示: ${'请认真填写信息'}`,
-      })
-    } else {
+    if (pass) {
       login(newUser).then(value => {
         ElMessage({
           type: 'success',
@@ -241,11 +162,174 @@ const singIn = () => {
           message: `提示: ${reason.data.message}`,
         })
       })
+    } else {
+      ElMessage({
+        type: 'info',
+        message: `提示: 请重新填写`,
+      })
     }
   });
 }
 
 const signUp = () => {
-  register(newUser);
+  form.value.validate(pass => {
+    if (pass) {
+      register(newUser).then(value => {
+        ElMessage({
+          type: 'success',
+          message: `提示: ${value.data.message}`,
+        })
+      }, reason => {
+        ElMessage({
+          type: 'info',
+          message: `提示: ${reason.data.message}`,
+        })
+      })
+    } else {
+      ElMessage({
+        type: 'info',
+        message: `提示: 请重新填写`,
+      })
+    }
+  });
 }
 </script>
+
+<template>
+  <!--  登录-->
+  <div id="userForm">
+    <div class="Ad-word">
+      <h1 :style="ad_word_h1">
+        欢迎{{ isLogin ? '登录' : '注册' }}XX音乐
+      </h1>
+      <h3 :style="/*{position:'fixed',bottom:bot}*/ad_word_h3">烦恼时，放松自己。</h3>
+
+    </div>
+    <el-form
+        ref="form"
+        style="max-width: 600px"
+        :model="newUser"
+        :rules="rules"
+        label-width="auto"
+        class="login"
+        :size="formSize"
+        status-icon
+        v-if="isLogin"
+    >
+
+      <el-form-item prop="account">
+        <el-input
+            v-model="newUser.account"
+            placeholder="请输入你的账号"
+            clearable
+
+        />
+      </el-form-item>
+
+
+      <el-form-item prop="password">
+        <el-input
+            v-model="newUser.password"
+            style="width: 240px"
+            type="password"
+            placeholder="请输入你的密码"
+            show-password
+            clearable
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="singIn" :disabled="unable">登录</el-button>
+        <el-button @click="toggle">返回注册</el-button>
+      </el-form-item>
+    </el-form>
+
+    <!--注册-->
+    <el-form
+        ref="form"
+        style="max-width: 600px"
+        :model="newUser"
+        :rules="rules"
+        label-width="auto"
+        class="registry"
+        :size="formSize"
+        status-icon
+        v-if="!isLogin"
+    >
+      <el-form-item prop="nickname">
+        <el-input
+            v-model="newUser.nickname"
+            style="width: 240px"
+            type="text"
+            placeholder="昵称"
+            clearable
+        />
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <el-input
+            v-model="newUser.password"
+            style="width: 240px"
+            type="password"
+            placeholder="请输入你的密码"
+            show-password
+            clearable
+        />
+      </el-form-item>
+
+      <el-form-item prop="rePassword">
+        <el-input
+            v-model="newUser.rePassword"
+            style="width: 240px"
+            type="password"
+            placeholder="再次输入你的密码"
+            show-password
+            clearable
+        />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="signUp">注册</el-button>
+        <el-button @click="toggle">返回登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+
+<style scoped>
+
+#userForm {
+  position: fixed;
+  bottom: 38%;
+  left: 40%;
+}
+
+#userForm .login {
+
+}
+
+#userForm .registry {
+
+
+}
+
+.Ad-word {
+
+}
+
+.Ad-word h3 {
+  position: fixed;
+  bottom: 550px;
+  /*bottom: 500px;*/
+
+}
+
+
+.Ad-word h1 {
+  position: fixed;
+  bottom: 600px;
+  /*bottom: 550px;*/
+}
+</style>
+
+
